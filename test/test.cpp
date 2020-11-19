35b1141bca858d867e48048fadbee9efa2cab08d
Daniil <umarmungenfuerimmer@gmail.com>
2020-11-19 14:43:36 +0400

    Implemented tests for capacity members

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN
 
-#include <catch.hpp>
 #include <algorithm>
+#include <catch.hpp>
 
 #include "fixed_string.hpp"
 
@@ -19,17 +19,14 @@ using u32fs = fixed_u32string<default_size>;
 
 namespace utils
 {
-template<typename T>
+template <typename T>
 constexpr auto equals(T value)
 {
-    return [v = std::move(value)](const T &a)
-    {
-        return a == v;
-    };
+    return [v = std::move(value)](const T &a) { return a == v; };
 }
 
 template <typename T, std::size_t N>
-using array_ref = T(&)[N];
+using array_ref = T (&)[N];
 
 template <std::size_t N>
 using literal_ref = array_ref<const char, N>;
@@ -38,11 +35,10 @@ template <typename TDestination, std::size_t N>
 void convert_literal(literal_ref<N> from, array_ref<TDestination, N> to)
 {
     std::transform(std::begin(from), std::end(from), std::begin(to),
-                   [](const auto a) { return static_cast<TDestination>(a); }
-    );
+                   [](const auto a) { return static_cast<TDestination>(a); });
 }
 
-}
+} // namespace utils
 
 namespace construction
 {
@@ -120,15 +116,17 @@ TEST_CASE("Construction")
         SECTION("fixed_string") { check<fs>(); }
         SECTION("fixed_wstring") { check<wfs>(); }
 #if FIXSTR_CPP20_CHAR8T_PRESENT
-            SECTION("fixed_u8string") { check<u8fs>(); }
+        SECTION("fixed_u8string") { check<u8fs>(); }
 #endif // FIXSTR_CPP20_CHAR8T_PRESENT
         SECTION("fixed_u16string") { check<u16fs>(); }
         SECTION("fixed_u32string") { check<u32fs>(); }
     }
 }
 
-namespace assignment {
-namespace from_c_array {
+namespace assignment
+{
+namespace from_c_array
+{
 template <typename T>
 void check()
 {
@@ -137,14 +135,14 @@ void check()
     char_t converted_literal[std::size(literal)]{};
     utils::convert_literal(literal, converted_literal);
 
-
     T str;
     REQUIRE(!std::equal(str.begin(), str.end(), std::begin(converted_literal)));
     str = converted_literal;
     REQUIRE(std::equal(str.begin(), str.end(), std::begin(converted_literal)));
 }
 } // namespace from_c_array
-namespace from_fixed_string {
+namespace from_fixed_string
+{
 template <typename T>
 void check()
 {
@@ -171,7 +169,7 @@ TEST_CASE("Assignment")
         SECTION("fixed_string") { check<fs>(); }
         SECTION("fixed_wstring") { check<wfs>(); }
 #if FIXSTR_CPP20_CHAR8T_PRESENT
-            SECTION("fixed_u8string") { check<u8fs>(); }
+        SECTION("fixed_u8string") { check<u8fs>(); }
 #endif // FIXSTR_CPP20_CHAR8T_PRESENT
         SECTION("fixed_u16string") { check<u16fs>(); }
         SECTION("fixed_u32string") { check<u32fs>(); }
@@ -182,9 +180,103 @@ TEST_CASE("Assignment")
         SECTION("fixed_string") { check<fs>(); }
         SECTION("fixed_wstring") { check<wfs>(); }
 #if FIXSTR_CPP20_CHAR8T_PRESENT
-            SECTION("fixed_u8string") { check<u8fs>(); }
+        SECTION("fixed_u8string") { check<u8fs>(); }
 #endif // FIXSTR_CPP20_CHAR8T_PRESENT
         SECTION("fixed_u16string") { check<u16fs>(); }
         SECTION("fixed_u32string") { check<u32fs>(); }
+    }
+}
+
+namespace capacity
+{
+namespace size
+{
+template <typename T>
+void check()
+{
+    T str;
+    REQUIRE(str.size() == default_size);
+}
+} // namespace size
+
+namespace max_size
+{
+template <typename T>
+void check()
+{
+    T str;
+    REQUIRE(str.max_size() == default_size);
+}
+} // namespace max_size
+
+namespace length
+{
+template <typename T>
+void check()
+{
+    T str;
+    REQUIRE(str.length() == default_size);
+}
+} // namespace length
+
+namespace empty
+{
+template <template <std::size_t> class T>
+void check()
+{
+    T<0> empty;
+    T<1> non_empty;
+    REQUIRE(empty.empty());
+    REQUIRE(!non_empty.empty());
+}
+} // namespace empty
+} // namespace capacity
+
+TEST_CASE("Capacity members")
+{
+    using namespace capacity;
+    SECTION("size")
+    {
+        using namespace size;
+        SECTION("fixed_string") { check<fs>(); }
+        SECTION("fixed_wstring") { check<wfs>(); }
+#if FIXSTR_CPP20_CHAR8T_PRESENT
+        SECTION("fixed_u8string") { check<u8fs>(); }
+#endif // FIXSTR_CPP20_CHAR8T_PRESENT
+        SECTION("fixed_u16string") { check<u16fs>(); }
+        SECTION("fixed_u32string") { check<u32fs>(); }
+    }
+    SECTION("max_size")
+    {
+        using namespace max_size;
+        SECTION("fixed_string") { check<fs>(); }
+        SECTION("fixed_wstring") { check<wfs>(); }
+#if FIXSTR_CPP20_CHAR8T_PRESENT
+        SECTION("fixed_u8string") { check<u8fs>(); }
+#endif // FIXSTR_CPP20_CHAR8T_PRESENT
+        SECTION("fixed_u16string") { check<u16fs>(); }
+        SECTION("fixed_u32string") { check<u32fs>(); }
+    }
+    SECTION("length")
+    {
+        using namespace length;
+        SECTION("fixed_string") { check<fs>(); }
+        SECTION("fixed_wstring") { check<wfs>(); }
+#if FIXSTR_CPP20_CHAR8T_PRESENT
+        SECTION("fixed_u8string") { check<u8fs>(); }
+#endif // FIXSTR_CPP20_CHAR8T_PRESENT
+        SECTION("fixed_u16string") { check<u16fs>(); }
+        SECTION("fixed_u32string") { check<u32fs>(); }
+    }
+    SECTION("length")
+    {
+        using namespace empty;
+        SECTION("fixed_string") { check<fixed_string>(); }
+        SECTION("fixed_wstring") { check<fixed_wstring>(); }
+#if FIXSTR_CPP20_CHAR8T_PRESENT
+        SECTION("fixed_u8string") { check<fixed_u8string>(); }
+#endif // FIXSTR_CPP20_CHAR8T_PRESENT
+        SECTION("fixed_u16string") { check<fixed_u16string>(); }
+        SECTION("fixed_u32string") { check<fixed_u32string>(); }
     }
 }
