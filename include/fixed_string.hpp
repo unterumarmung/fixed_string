9c7ee30afd3fb28783c0fe06f3aa94ec91dc4698
Daniil <umarmungenfuerimmer@gmail.com>
2021-08-15 22:56:00 +0300

    try to fix

--- include/fixed_string.hpp
+++ include/fixed_string.hpp
@@ -222,7 +222,7 @@ struct basic_fixed_string
 
     // clang-format off
     template <size_type pos = 0, size_type count = npos,
-              typename..., bool IsPosInBounds = pos <= N, typename = std::enable_if_t<IsPosInBounds>>
+              typename..., bool IsPosInBounds = pos <= N, std::enable_if_t<IsPosInBounds>* = nullptr>
     [[nodiscard]] constexpr auto substr() noexcept
         -> substr_result_type<pos, count>
     // clang-format on
