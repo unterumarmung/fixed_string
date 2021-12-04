4d55f6bc2710834fdc87a5b45bfb2f443e3e53a0
Daniil <umarmungenfuerimmer@gmail.com>
2021-12-04 19:21:48 +0300

    .

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -16,6 +16,7 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">🚧Under construction</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
